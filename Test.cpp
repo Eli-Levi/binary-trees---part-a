6b19d5ec07bfdeec9ca216a9c473ac85072ac2f8
Eli-Levi <el1l3vi@gmail.com>
2021-05-26 16:04:11 +0300

    ...

--- Test.cpp
+++ Test.cpp
@@ -9,44 +9,51 @@ using namespace std;
 TEST_CASE("valid empty case")
 {
     BinaryTree<int> tree;
-    CHECK_EQ(tree.get_root(), nullptr);
+    CHECK(tree.get_root() == nullptr);
 }
 
 TEST_CASE("adding root properly")
 {
+    cout << "bla\n";
     BinaryTree<int> tree;
-    tree.add_root(5);
-    CHECK_EQ(tree.get_root()->value, 5);
+    cout << "bla bla\n";
+    CHECK_NOTHROW(tree.add_root(5));
+    cout << "bla bla bla\n";
+
+    CHECK(tree.get_root()->value == 5);
+    cout << "bla bla bla bla\n";
 
     BinaryTree<string> stree;
+        cout << "bla bla bla bla bla\n";
+
     stree.add_root("hello");
-    CHECK_EQ(stree.get_root()->value, "hello");
+    CHECK(stree.get_root()->value == "hello");
 }
 
 TEST_CASE("replacing current root")
 {
     BinaryTree<int> tree;
     tree.add_root(5);
-    tree.add_root(20);
-    CHECK_EQ(tree.get_root()->value, 20);
+    CHECK_NOTHROW(tree.add_root(20));
+    CHECK(tree.get_root()->value == 20);
 
     BinaryTree<string> stree;
     stree.add_root("hello");
     stree.add_root("world");
-    CHECK_EQ(stree.get_root()->value, "world");
+    CHECK(stree.get_root()->value == "world");
 }
 
 TEST_CASE("adding left child")
 {
     BinaryTree<int> tree;
     tree.add_root(5);
-    tree.add_left(tree.get_root()->value, 20);
-    CHECK_EQ(tree.get_left(tree.get_root())->value, 20);
+    CHECK_NOTHROW(tree.add_left(tree.get_root()->value, 20));
+    CHECK(tree.get_left(tree.get_root())->value == 20);
 
     BinaryTree<string> stree;
     stree.add_root("hello");
     stree.add_left(stree.get_root()->value, "world");
-    CHECK_EQ(stree.get_left(stree.get_root())->value, "world");
+    CHECK(stree.get_left(stree.get_root())->value == "world");
 }
 
 TEST_CASE("replacing left child")
@@ -54,27 +61,27 @@ TEST_CASE("replacing left child")
     BinaryTree<int> tree;
     tree.add_root(5);
     tree.add_left(tree.get_root()->value, 20);
-    tree.add_left(tree.get_root()->value, 80);
-    CHECK_EQ(tree.get_left(tree.get_root())->value, 80);
+    CHECK_NOTHROW(tree.add_left(tree.get_root()->value, 80));
+    CHECK(tree.get_left(tree.get_root())->value == 80);
 
     BinaryTree<string> stree;
     stree.add_root("hello");
     stree.add_left(stree.get_root()->value, "world");
     stree.add_left(stree.get_root()->value, "there");
-    CHECK_EQ(stree.get_left(stree.get_root())->value, "there");
+    CHECK(stree.get_left(stree.get_root())->value == "there");
 }
 
 TEST_CASE("adding right child")
 {
     BinaryTree<int> tree;
     tree.add_root(5);
-    tree.add_right(tree.get_root()->value, 99);
-    CHECK_EQ(tree.get_right(tree.get_root())->value, 99);
+    CHECK_NOTHROW(tree.add_right(tree.get_root()->value, 99));
+    CHECK(tree.get_right(tree.get_root())->value == 99);
 
     BinaryTree<string> stree;
     stree.add_root("hello");
     stree.add_right(stree.get_root()->value, "you");
-    CHECK_EQ(stree.get_right(stree.get_root())->value, "you");
+    CHECK(stree.get_right(stree.get_root())->value == "you");
 }
 
 TEST_CASE("invalid input: adding right child to non existing root")
@@ -83,16 +90,16 @@ TEST_CASE("invalid input: adding right child to non existing root")
     CHECK_THROWS(tree.add_right(tree.get_root()->value, 99));
 
     BinaryTree<string> stree;
-    CHECK_THROWS(stree.add_right(stree.get_root()->value , "you"));
+    CHECK_THROWS(stree.add_right(stree.get_root()->value, "you"));
 }
 
 TEST_CASE("invalid input: adding left child to non existing root")
 {
     BinaryTree<int> tree;
-    CHECK_THROWS(tree.add_left(tree.get_root()->value , 42));
+    CHECK_THROWS(tree.add_left(tree.get_root()->value, 42));
 
     BinaryTree<string> stree;
-    CHECK_THROWS(stree.add_left(stree.get_root()->value , "you"));
+    CHECK_THROWS(stree.add_left(stree.get_root()->value, "you"));
 }
 
 /*TEST_CASE("invalid input: adding right child to non existing node")
@@ -100,7 +107,7 @@ TEST_CASE("invalid input: adding left child to non existing root")
     BinaryTree<int> tree;
     tree.add_root(-7);
     tree.add_right(tree.get_root()->value, 99);
-    CHECK_THROWS(87, 420);
+    CHECK_THROWS(100, 420);
 
     BinaryTree<string> stree;
     stree.add_root("yo");
